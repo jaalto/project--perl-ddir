331f3ebcad925536da9cf2162e3ca74c971fa5c8
Jari Aalto <jari.aalto@cante.net>
2010-03-15 12:25:50 +0200

    bin/ddir.pl: Complete facelift
    Signed-off-by: Jari Aalto <jari.aalto@cante.net>

--- bin/ddir.pl
+++ bin/ddir.pl
@@ -1,28 +1,30 @@
 #!/usr/bin/perl
-'di';
-'ig00';
-
-# ddir.pl -- Display DOS style hierarchical directory tree
+#
+#   copyright-update -- Utility to update copyright information in files
+#
+#   Copyright
 #
 #       Copyright (C) 1995-2010 Jari Aalto
 #       Copyright (C) 1994 Brian Blackmore
 #
-#       This program is free software; you can redistribute it and/or
-#       modify it under the terms of the GNU General Public License as
-#       published by the Free Software Foundation; either version 2 of
-#       the License, or (at your option) any later version.
+#   License
 #
-#       This program is distributed in the hope that it will be useful, but
-#       WITHOUT ANY WARRANTY; without even the implied warranty of
-#       MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-#       General Public License for more details.
+#       This program is free software; you can redistribute it and/or modify
+#       it under the terms of the GNU General Public License as published by
+#       the Free Software Foundation; either version 2 of the License, or
+#       (at your option) any later version.
+#
+#       This program is distributed in the hope that it will be useful,
+#       but WITHOUT ANY WARRANTY; without even the implied warranty of
+#       MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#       GNU General Public License for more details.
 #
 #       You should have received a copy of the GNU General Public License
 #       along with this program. If not, see <http://www.gnu.org/licenses/>.
 #
-#       Visit <http://www.gnu.org/copyleft/gpl.html> for more information
+#   Documentation
 #
-#   Description
+#       To read manual, start this program with option: --help
 #
 #       Origianally by Brian Blackmore. Modernized by Jari Aalto
 
@@ -32,7 +34,15 @@
 #
 # ****************************************************************************
 
+use strict;
+
+use autouse 'Pod::Text'     => qw( pod2text );
+use autouse 'Pod::Html'     => qw( pod2html );
+
 use English qw( -no_match_vars );
+use Getopt::Long;
+use File::Basename;
+#use File::Find;
 
 # ****************************************************************************
 #
@@ -47,7 +57,11 @@ use vars qw ( $VERSION );
 #   The following variable is updated by custom Emacs setup whenever
 #   this file is saved.
 
-my $VERSION = '2010.0315.0958';
+my $VERSION = '2010.0315.1025';
+
+my $DEFAULT_PATH_EXCLUDE = ''		# Matches *only path component
+    . '(CVS|RCS|\.(bzr|svn|git|darcs|arch|mtn|hg))$'
+    ;
 
 # ****************************************************************************
 #
@@ -90,6 +104,273 @@ sub Initialize ()
 #
 #   DESCRIPTION
 #
+#       Help function and embedded POD documentation
+#
+#   INPUT PARAMETERS
+#
+#       none
+#
+#   RETURN VALUES
+#
+#       none
+#
+# ****************************************************************************
+
+=pod
+
+=head1 NAME
+
+ddir - Display DOS style hierarchical directory tree
+
+=head1 SYNOPSIS
+
+  ddir [options] DIR
+
+=head1 DESCRIPTION
+
+Display an indented directory tree, using ASCII graphical character to
+represent hierarchical sturucture. The directories to include or
+exlude can be speficied with command line options. A Perl
+implementation of the tree(1) program.
+
+=head1 OPTIONS
+
+=over 4
+
+=item B<-d, --dir>
+
+Display only directories.
+
+=item B<-h, --help>
+
+Print text help
+
+=item B<--help-exclude>
+
+Print default exclude path value when B<--exclude-vcs> is used.
+
+=item B<--help-html>
+
+Print help in HTML format.
+
+=item B<--help-man>
+
+Print help in manual page C<man(1)> format.
+
+=item B<-i, --include REGEXP>
+
+Include files mathing regexp. The match is done against whole path. The option
+can be used multiple times.
+
+If this option is not supplied, every file is automatically included.
+The matches can be further filtered by using options B<--exclude>.
+
+=item B<-v, --verbose LEVEL>
+
+Print informational messages. Increase numeric LEVEL for more
+verbosity.
+
+=item B<-V, --version>
+
+Print contact and version information.
+
+=item B<-x, --exclude REGEXP>
+
+Ignore files mathing regexp. The match is done against whole path. The option
+can be used multiple times.
+
+This option is applied after possible B<--include> matches.
+
+=item B<-C, --exclude-vcs>
+
+Exclude version control directories. See B<--help-exclude>.
+
+=back
+
+=head1 EXAMPLES
+
+Show directory tree by excluding version control directories. Display
+only directories:
+
+    ddir --x-vcs --dir .
+
+    .
+    +--doc/
+    |  +--manual/
+    +--bin/
+
+=head1 TROUBLESHOOTING
+
+None.
+
+=head1 EXAMPLES
+
+None.
+
+=head1 ENVIRONMENT
+
+=head1 FILES
+
+None.
+
+=head1 SEE ALSO
+
+tree(1)
+
+=head1 COREQUISITES
+
+Uses standard Perl modules.
+
+=head1 AVAILABILITY
+
+Homepage is at http://freshmeat.net/projects/ddir
+
+=head1 AUTHORS
+
+Copyright (C) 1995-2010 Jari Aalto.
+Copyright (C) 1994 Brian Blackmore.
+
+=head1 LICENSE
+
+This program is free software; you can redistribute and/or modify
+program under the terms of GNU General Public license either version 2
+of the License, or (at your option) any later version.
+
+=cut
+
+sub Help (;$$)
+{
+    my $id   = "$LIB.Help";
+    my $type = shift;  # optional arg, type
+    my $msg  = shift;  # optional arg, why are we here...
+
+    if ( $type eq -html )
+    {
+        pod2html $PROGRAM_NAME;
+    }
+    elsif ( $type eq -man )
+    {
+	eval "use Pod::Man;";
+        $EVAL_ERROR  and  die "$id: Cannot generate Man: $EVAL_ERROR";
+
+        my %options;
+        $options{center} = 'cvs status - formatter';
+
+        my $parser = Pod::Man->new(%options);
+        $parser->parse_from_file ($PROGRAM_NAME);
+    }
+    else
+    {
+	if ( $^V =~ /5\.10/ )
+	{
+	    # Bug in 5.10. Cant use string ("") as a symbol ref
+	    # while "strict refs" in use at
+	    # /usr/share/perl/5.10/Pod/Text.pm line 249.
+
+	    system("pod2text $PROGRAM_NAME");
+	}
+	else
+	{
+	    pod2text $PROGRAM_NAME;
+	}
+    }
+
+    defined $msg  and  print $msg;
+    exit 0;
+}
+
+# ****************************************************************************
+#
+#   DESCRIPTION
+#
+#       Return current year YYYY
+#
+#   INPUT PARAMETERS
+#
+#       None
+#
+#   RETURN VALUES
+#
+#       number      YYYY
+#
+# ****************************************************************************
+
+sub HelpExclude ()
+{
+    my $id = "$LIB.HelpExclude";
+
+    print "Default path exclude regexp: '$DEFAULT_PATH_EXCLUDE'\n";
+}
+
+# ****************************************************************************
+#
+#   DESCRIPTION
+#
+#       Read command line arguments and their parameters.
+#
+#   INPUT PARAMETERS
+#
+#       None
+#
+#   RETURN VALUES
+#
+#       Globally set options.
+#
+# ****************************************************************************
+
+sub HandleCommandLineArgs ()
+{
+    my $id = "$LIB.HandleCommandLineArgs";
+
+    use vars qw
+    (
+        $test
+        $verb
+        $debug
+        @OPT_FILE_REGEXP_EXCLUDE
+        $OPT_EXCLUDE_VCS
+    );
+
+    Getopt::Long::config( qw
+    (
+        require_order
+        no_ignore_case
+        no_ignore_case_always
+    ));
+
+    my ( $help, $helpMan, $helpHtml, $version ); # local variables to function
+    my ( $helpExclude, $excludeVcs );
+
+    $debug = -1;
+
+    GetOptions      # Getopt::Long
+    (
+	  "d|debug:i"	    => \$debug
+	, "help-exclude"    => \$helpExclude
+	, "help-html"	    => \$helpHtml
+	, "help-man"	    => \$helpMan
+	, "h|help"	    => \$help
+	, "v|verbose:i"	    => \$verb
+	, "V|version"	    => \$version
+	, "x|exclude=s"	    => \@OPT_FILE_REGEXP_EXCLUDE
+	, "X|exclude-vcs"   => \$OPT_EXCLUDE_VCS
+    );
+
+    $version		and  die "$VERSION $CONTACT $LICENSE $URL\n";
+    $helpExclude 	and  HelpExclude();
+    $help		and  Help();
+    $helpMan		and  Help(-man);
+    $helpHtml		and  Help(-html);
+    $version		and  Version();
+
+    $debug = 1          if $debug == 0;
+    $debug = 0          if $debug < 0;
+}
+
+# ****************************************************************************
+#
+#   DESCRIPTION
+#
 #       Resolve a pathname into its shortest version Removing any
 #       references to the directory ".", any references to // , any
 #       references to directory/.. and any final /
@@ -105,7 +386,7 @@ sub Initialize ()
 #
 # ****************************************************************************
 
-sub resolve ($$)
+sub Resolve ($$)
 {
     my ( $file, $direct ) = @ARG;
 
@@ -141,9 +422,9 @@ sub resolve ($$)
 #
 # ****************************************************************************
 
-sub tree ($$);   # Forward declaration for recursive use.
+sub Tree ($$);   # Forward declaration for recursive use.
 
-sub tree ($$)
+sub Tree ($$)
 {
     my ( $dir, $level ) = @ARG;
 
@@ -156,16 +437,16 @@ sub tree ($$)
     my @files = readdir DIRECT ;
     local $ARG;
 
-    while ( $name = shift @files )
+    while ( my $name = shift @files )
     {
         #  Skip directories .  and  ..
         next if $name =~ /^\.\.?$/;
 
-        resolve $name, $dir;
+        Resolve $name, $dir;
 
         if ( -d )
         {
-            $newname = $ARG;
+            my $newname = $ARG;
 
             if ( -l $newname )
             {
@@ -182,11 +463,11 @@ sub tree ($$)
 
                 if ( @files )
                 {
-                    tree $newname,"$level|  ";
+                    Tree $newname, "$level|  ";
                 }
                 else
                 {
-                    tree $newname,"$level   ";
+                    Tree $newname, "$level   ";
                 }
             }
             else
@@ -196,7 +477,6 @@ sub tree ($$)
         }
     }
 }
-
 
 # ****************************************************************************
 #
@@ -216,41 +496,15 @@ sub tree ($$)
 
 sub Main ()
 {
-    $dir = $ARGV[0] || ".";
+    Initialize();
+    HandleCommandLineArgs();
+
+    my $dir = $ARGV[0] or die "Missing directory argument. See --help.\n";
 
     print "$dir\n";
-    tree $dir, "";
+    Tree $dir, "";
 }
 
 Main();
 
-##############################################################################
-
-        # These next few lines are legal in both Perl and nroff.
-
-.00;                    # finish .ig
-
-'di                     \" finish diversion--previous line must be blank
-.nr nl 0-1              \" fake up transition to first page again
-.nr % 0                 \" start at page 1
-'; __END__ ############# From here on it's a standard manual page ############
-.TH DDIR 1 "March 9, 1994"
-.AT 3
-.SH NAME
-ddir \- Program to do a DOS like directory tree
-.SH SYNOPSIS
-.B ddir
-.SH DESCRIPTION
-.I Ddir
-Prints out only directories, not files while recursing down from the
-current directory. This is quite slow program to use...
-.SH ENVIRONMENT
-No environment variables are used.
-.SH FILES
-None.
-.SH AUTHOR
-Brian Blackmore
-.SH "SEE ALSO"
-.SH DIAGNOSTICS
-.SH BUGS
-.ex
+# End of file
